cdec0e169af03b8387dd32537059dc564495c220
Alfisan <goldalfi@gmail.com>
2019-08-11 16:13:12 +0700

    Create Screen Home and detail packet, create component pomList

--- App/Navigation/AppNavigation.js
+++ App/Navigation/AppNavigation.js
@@ -1,10 +1,14 @@
 import { createStackNavigator, createAppContainer } from 'react-navigation'
+import DetailPaketScreen from '../Containers/DetailPaketScreen'
+import HomeScreen from '../Containers/HomeScreen'
 import LaunchScreen from '../Containers/LaunchScreen'
 
 import styles from './Styles/NavigationStyles'
 
 // Manifest of possible screens
 const PrimaryNav = createStackNavigator({
+  DetailPaketScreen: { screen: DetailPaketScreen },
+  HomeScreen: { screen: HomeScreen },
   LaunchScreen: { screen: LaunchScreen }
 }, {
   // Default config for all screens
